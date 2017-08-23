546f7eaba9025f9fde9cfd24f9ea7844863a43d6
eaytac8 <elif.aytc@hotmail.com>
2017-08-23 11:13:39 +0300

    tema giydirildi. örnek logo eklendi. henüz veritabanı yok.

---
+++ EgtMerkez/EgtMerkez/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace EgtMerkez
+{
+    public class BundleConfig
+    {
+         public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.min.js","~/Scripts/plugings.js","~/Scripts/custom.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/jquery-1.10.2.min.js", "~/Scripts/jquery-migrate-1.2.1.min.js", "~/Scripts/modernizr.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/font.awesome.min.css", "~/Content/css/animate.css", "~/Content/css/style.css"));
+        }
+    }
+}
---
+++ EgtMerkez/EgtMerkez/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EgtMerkez.MvcApplication" Language="C#" %>
